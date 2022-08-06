3b96051cdfc8d3d8708a86b6d91e894e573cfc3b
Maickl-Denis <81251154+Maickl-Denis@users.noreply.github.com>
2022-08-06 15:46:32 +0300

    первое занятие с лектором

--- less1.0-001/Program.cs
+++ less1.0-001/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Console.Write("введите число: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int result = num * num;
+// int sqrt = Convert.ToInt32(Math.Pow(num, 2));
+// num *= num;
+
+// Console.WriteLine("Число в квадрате: " + result);
+// Console.WriteLine("Число в квадрате: " + sqrt);
+// Console.WriteLine("Число в квадрате: " + num);
+
+// //вторая задача
+// Console.Write("введите первое число: ");
+// int num0 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите второе число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if (num0 == num1*num1)
+// {
+//     Console.Write("Первое число квад")
+// }
+// else
+// {
+//     Console.Write("Первое число не квад")
+// }
+
+
+//#########################################
+
+//третья программа
+// Console.Write("введите день: ");
+
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7)
+// {
+//     Console.Write("таких дней нет");
+// }
+// {
+//     if (day == 1)
+//     {
+//         Console.Write("Monday");
+//     }
+//     if (day == 2)
+//     {
+//         Console.Write("Вторник");
+//     }
+
+// }
+
+//#######################################
+// //задача  4
+// Console.Write("введите число: ");
+
+// int day = Convert.ToInt32(Console.ReadLine());
+// int day1 = -day;
+// Range myRange1 = day1..day;
+// Console.Write(myRange1);
+// 4 Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int B = -N;
+// while (B <= N)
+// {
+//     Console.Write(B + " ");
+//     B++; // B = B + 1 аналогично B += 1 
+// }
+//#########################################
+
+
+// 5 Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int y = num % 10;
+// Console.Write(y);
+
+//#########################################
+
