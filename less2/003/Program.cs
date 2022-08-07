8d32c3bf6cf595c67283864b12b9a57daf7a001a
Maickl-Denis <81251154+Maickl-Denis@users.noreply.github.com>
2022-08-07 13:54:09 +0300

    Второе заняетие

---
+++ less2/003/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] array = {1, 12, 18, 31, 4, 15, 16, 17, 18};
+
+int len = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < len)
+{
+    
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
---
+++ less2/004/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArrey(int[] collection)
+{
+    int ln = collection.Length;
+    int index = 0;
+    while (index < ln)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArrey(int[] col)
+{
+    int count =col.Length;
+    int posiion = 0;
+    while(posiion < count)
+    {
+        Console.Write(col[posiion]);
+        posiion++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int [] arrey = new int[10];
+
+
+FillArrey(arrey);
+PrintArrey(arrey);
+Console.WriteLine();
+
+
+
+int pos = IndexOff(arrey, 4);
+Console.WriteLine(pos);
