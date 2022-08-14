6acf86aba0eaf78eeb6e2baa08cc7eb3c562004e
Maickl-Denis <81251154+Maickl-Denis@users.noreply.github.com>
2022-08-14 17:09:27 +0300

    довлена лекция 3.0 функции, циклы

---
+++ less3.0/001/Program.cs
@@ -0,0 +1,78 @@
+﻿void Method1()
+    {
+        Console.WriteLine("Автор...");
+    }
+
+
+Method1();
+
+//вид2
+
+void Method2(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+
+Method2(msg: "текст сообщения");
+
+void Method21(string msg, int count)
+    {
+        int i = 0;
+        while (i < count)
+        {
+            Console.WriteLine(msg);
+            i++;
+        }
+    }
+
+Method21(msg: "Текст", count: 4);
+
+//вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int Year = Method3();
+Console.WriteLine(Year);
+
+
+//вид 4
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+    
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string text)
+{
+
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
+
+for(int i = 2; i <= 10; i++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
---
+++ less3.0/003/Program.cs
@@ -0,0 +1,31 @@
+﻿int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelSort(int [] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPos = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPos]) minPos = j; 
+        }
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+PrintArray(arr);
+SelSort(arr);
+PrintArray(arr);
